d2562aa45aaff6b09c225de6f65715e954f65d4c
hui-aqua <50576083+hui-aqua@users.noreply.github.com>
2019-10-25 18:09:45 +0200

    change the IdeLinwave to Airywave add stokes second order wave theory

--- enviroments/seacondition.py
+++ enviroments/seacondition.py
@@ -7,9 +7,9 @@ import numpy as np
 from numpy import pi
 
 
-class IdeLinwave:
+class Airywave:
     """
-    Using idealized linear waves theory      \n
+    Using Airy wave theory      \n
     To define this class, need to import:    \n
     waveheight [m], wavelength [m], waveperiod [s], waterdepth [m] \n
     in this library, I assume the wave is coming from -X, and long X-axis,
@@ -18,30 +18,40 @@ class IdeLinwave:
     """
 
     def __init__(self, waveHeight=1.0, waveLength=25.0, waterDepth=60.0):
+        self.gravity = 9.81
         self.waveHeight = waveHeight
         self.waveLength = waveLength
-        self.wavePeriod = np.sqrt(2 * pi * waveLength / 9.81)
         self.waterDepth = waterDepth
-        self.wavek = 2 * np.pi / self.waveLength
-        self.piht = np.pi * self.waveHeight / self.wavePeriod
-        self.piht2 = 2 * self.waveHeight * pow(pi / self.wavePeriod, 2)
+        self.phasevelocity = np.sqrt(self.gravity * waveLength * np.tanh(2 * pi * waterDepth / waveLength) / (2.0 * pi))
+        self.wavePeriod = waveLength / self.phasevelocity
+        self.wavek = 2 * pi / waveLength
+        self.piht = pi * waveHeight / self.wavePeriod
+        self.pihl = pi * waveHeight / waveLength
+        self.piht2 = 2 * waveHeight * pow(pi / self.wavePeriod, 2)
 
     def __str__(self):
-        s0 = 'The environment is idealized linear wave condition and the specific parameters are:\n'
+        s0 = 'The environment is airy wave (deep water) wave condition and the specific parameters are:\n'
         s1 = 'water Depth= ' + str(self.waterDepth) + ' m\n'
         s2 = 'wave Period= ' + str(self.wavePeriod) + ' s\n'
+        s5 = 'wave Period(G)= ' + str(np.sqrt(2 * pi * self.waveLength / 9.81)) + ' s\n'
         s3 = 'wave Length= ' + str(self.waveLength) + ' m\n'
         s4 = 'wave Height= ' + str(self.waveHeight) + ' m\n'
-        S = s0 + s1 + s2 + s3 + s4
+        S = s0 + s1 + s2 + s3 + s4 + s5
         return S
 
     def Get_Velo(self, posi, time):
-        s = posi[2] + self.waterDepth
-        zeta = self.wavek * posi[0] - 2 * np.pi / self.wavePeriod * time
+        zeta = self.wavek * posi[0] - 2 * pi / self.wavePeriod * time
         yita = self.Get_Surface(posi, time)
         if posi[2] < yita:
-            horizonvelocity = self.piht * np.cosh(self.wavek * s) * np.cos(zeta) / np.sinh(self.wavek * self.waterDepth)
-            vericalvelocity = self.piht * np.sinh(self.wavek * s) * np.sin(zeta) / np.sinh(self.wavek * self.waterDepth)
+            horizonvelocity = self.piht * np.cosh(self.wavek * (posi[2] + self.waterDepth)) * np.cos(zeta) / np.sinh(
+                self.wavek * self.waterDepth)
+            vericalvelocity = self.piht * np.sinh(self.wavek * (posi[2] + self.waterDepth)) * np.sin(zeta) / np.sinh(
+                self.wavek * self.waterDepth)
+
+            if self.waterDepth > self.waveLength * 0.5:  # if it is deep water
+                print("here")
+                horizonvelocity = self.piht * np.exp(self.wavek * posi[2]) * np.cos(zeta)
+                vericalvelocity = self.piht * np.exp(self.wavek * posi[2]) * np.sin(zeta)
         else:
             horizonvelocity = 0.0
             vericalvelocity = 0.0
@@ -52,14 +62,16 @@ class IdeLinwave:
         return velo
 
     def Get_Acce(self, posi, time):
-        s = posi[2] + self.waterDepth
-        zeta = self.wavek * posi[0] - 2 * np.pi / self.wavePeriod * time
-        yita = self.waveHeight / 2 * np.cos(zeta)
+        zeta = self.wavek * posi[0] - 2 * pi / self.wavePeriod * time
+        yita = self.Get_Surface(posi, time)
         if posi[2] < yita:
-            horizontalacceleration = self.piht2 * np.cosh(self.wavek * s) * np.sin(zeta) / np.sinh(
-                self.wavek * self.waterDepth)
-            vericalaccelateration = -self.piht2 * np.sinh(self.wavek * s) * np.cos(zeta) / np.sinh(
-                self.wavek * self.waterDepth)
+            horizontalacceleration = self.piht2 * np.cosh(self.wavek * (posi[2] + self.waterDepth)) * np.sin(
+                zeta) / np.sinh(self.wavek * self.waterDepth)
+            vericalaccelateration = -self.piht2 * np.sinh(self.wavek * (posi[2] + self.waterDepth)) * np.cos(
+                zeta) / np.sinh(self.wavek * self.waterDepth)
+            if self.waterDepth > self.waveLength * 0.5:  # if it is deep water
+                horizontalacceleration = self.piht2 * np.exp(self.wavek * posi[2]) * np.sin(zeta)
+                vericalaccelateration = -self.piht2 * np.exp(self.wavek * posi[2]) * np.cos(zeta)
         else:
             horizontalacceleration = 0.0
             vericalaccelateration = 0.0
@@ -70,6 +82,84 @@ class IdeLinwave:
         return acce
 
     def Get_Surface(self, posi, time):
-        zeta = self.wavek * posi[0] - 2 * np.pi / self.wavePeriod * time
+        zeta = self.wavek * posi[0] - 2 * pi / self.wavePeriod * time
         yita = self.waveHeight / 2 * np.cos(zeta)
+        return yita
+
+
+class Stocks2wave(Airywave):
+    def __str__(self):
+        s0 = 'The environment is stokes second order wave theory condition and the specific parameters are:\n'
+        s1 = 'water Depth= ' + str(self.waterDepth) + ' m\n'
+        s3 = 'wave Length= ' + str(self.waveLength) + ' m\n'
+        s4 = 'wave Height= ' + str(self.waveHeight) + ' m\n'
+        S = s0 + s1 + s3 + s4
+        return S
+
+    def Get_Velo(self, posi, time):
+        zeta = self.wavek * posi[0] - 2 * pi / self.wavePeriod * time
+        yita = self.Get_Surface(posi, time)
+        if posi[2] < yita:
+            horizonvelocity = self.piht * np.cosh(self.wavek * (posi[2] + self.waterDepth)) * np.cos(zeta) / np.sinh(
+                self.wavek * self.waterDepth) + 0.75 * self.piht * self.pihl * np.cosh(
+                2 * self.wavek * (posi[2] + self.waterDepth)) * np.cos(2 * zeta) / pow(4, np.sinh(
+                self.wavek * self.waterDepth))
+            vericalvelocity = self.piht * np.sinh(self.wavek * (posi[2] + self.waterDepth)) * np.sin(zeta) / np.sinh(
+                self.wavek * self.waterDepth) + 0.75 * self.piht * self.pihl * np.sinh(
+                2 * self.wavek * (posi[2] + self.waterDepth)) * np.sin(2 * zeta) / pow(4, np.sinh(
+                self.wavek * self.waterDepth))
+            if self.waterDepth > self.waveLength * 0.5:  # if it is deep water
+                horizonvelocity = self.piht * np.exp(self.wavek * posi[2]) * np.cos(
+                    zeta) + 0.75 * self.piht * self.pihl * np.cosh(
+                    2 * self.wavek * (posi[2] + self.waterDepth)) * np.cos(2 * zeta) / pow(4, np.sinh(
+                    self.wavek * self.waterDepth))
+                vericalvelocity = self.piht * np.exp(self.wavek * posi[2]) * np.sin(
+                    zeta) + 0.75 * self.piht * self.pihl * np.sinh(
+                    2 * self.wavek * (posi[2] + self.waterDepth)) * np.sin(2 * zeta) / pow(4, np.sinh(
+                    self.wavek * self.waterDepth))
+        else:
+            horizonvelocity = 0.0
+            vericalvelocity = 0.0
+        velo = np.array([0.0, 0.0, 0.0])
+        velo[0] = horizonvelocity
+        velo[1] = 0.0
+        velo[2] = vericalvelocity
+        return velo
+
+    def Get_Acce(self, posi, time):
+        zeta = self.wavek * posi[0] - 2 * pi / self.wavePeriod * time
+        yita = self.Get_Surface(posi, time)
+        if posi[2] < yita:
+            horizontalacceleration = self.piht2 * np.cosh(self.wavek * (posi[2] + self.waterDepth)) * np.sin(
+                zeta) / np.sinh(self.wavek * self.waterDepth) + 1.5 * self.piht2 * self.pihl * np.cosh(
+                2 * self.wavek * (posi[2] + self.waterDepth)) * np.sin(2 * zeta) / pow(4, np.sinh(
+                self.wavek * self.waterDepth))
+            vericalaccelateration = -self.piht2 * np.sinh(self.wavek * (posi[2] + self.waterDepth)) * np.cos(
+                zeta) / np.sinh(self.wavek * self.waterDepth) - 1.5 * self.piht2 * self.pihl * np.sinh(
+                2 * self.wavek * (posi[2] + self.waterDepth)) * np.cos(2 * zeta) / pow(4, np.sinh(
+                self.wavek * self.waterDepth))
+            if self.waterDepth > self.waveLength * 0.5:  # if it is deep water
+                horizontalacceleration = self.piht2 * np.exp(self.wavek * posi[2]) * np.sin(
+                    zeta) + 1.5 * self.piht2 * self.pihl * np.cosh(
+                    2 * self.wavek * (posi[2] + self.waterDepth)) * np.sin(2 * zeta) / pow(4, np.sinh(
+                    self.wavek * self.waterDepth))
+                vericalaccelateration = -self.piht2 * np.exp(self.wavek * posi[2]) * np.cos(
+                    zeta) - 1.5 * self.piht2 * self.pihl * np.sinh(
+                    2 * self.wavek * (posi[2] + self.waterDepth)) * np.cos(2 * zeta) / pow(4, np.sinh(
+                    self.wavek * self.waterDepth))
+        else:
+            horizontalacceleration = 0.0
+            vericalaccelateration = 0.0
+        acce = np.array([0.0, 0.0, 0.0])
+        acce[0] = horizontalacceleration
+        acce[1] = 0.0
+        acce[2] = vericalaccelateration
+        return acce
+
+    def Get_Surface(self, posi, time):
+        zeta = self.wavek * posi[0] - 2 * pi / self.wavePeriod * time
+        yita = self.waveHeight / 2 * np.cos(
+            zeta) + pi * self.waveHeight * self.waveHeight / 8.0 / self.waveLength * np.cosh(
+            self.wavek * self.waterDepth) * (2 + np.cosh(2 * self.wavek * self.waterDepth)) * np.cos(2 * zeta) / pow(
+            np.sinh(self.wavek * self.waterDepth), 3)
         return yita
--- enviroments/showwave.py
+++ enviroments/showwave.py
@@ -1,21 +1,25 @@
 import seacondition as sea
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
-wave1 = sea.IdeLinwave(2, 25, 60)
-
+wave1 = sea.Airywave(4.91, 125, 16)
+wave2 = sea.Stocks2wave(4.91, 125, 16)
 print(wave1)
+print(wave2)
 t = 0
 posi = np.array((0, 0, 0))
-velo = wave1.Get_Velo(posi, t)
+velo = wave1.Get_Velo(posi, 1)
 print("velocity = ", velo)
 
 # 2D domino
-domix = np.linspace(0, 50, 2 * 50 + 1)
+
+domix = np.linspace(0, 500, 2 * 50 + 1)
 domiy = np.linspace(-10, 10, 21)
-domiz = np.linspace(0, -50, 0.2 * 50 + 1)
+domiz = np.linspace(0, -50, 10 + 1)
 T = 0
 wf = np.array((0))
+wf2 = np.array((0))
 x = []
 y = []
 z = []
@@ -24,24 +28,22 @@ Vx = []
 Vz = []
 for i in range(len(domix)):
     wf = np.vstack((wf, wave1.Get_Surface(np.array((domix[i], 0, 0)), T)))
+    wf2 = np.vstack((wf2, wave2.Get_Surface(np.array((domix[i], 0, 0)), T)))
     x.append(domix[i])
     y.append(domiy[10])
-    z.append(domiz[2])
-    posi = np.array((domix[i], domiy[10], domiz[2]))
-    print("posit=", posi)
-    print("velosity is ", wave1.Get_Velo(posi, T))
+    z.append(domiz[1])
+    posi = np.array((domix[i], domiy[10], domiz[1]))
     waveV.append(wave1.Get_Velo(posi, T))
+    Vx.append(wave1.Get_Velo(posi, T)[0])
+    Vz.append(wave1.Get_Velo(posi, T)[2])
 wf = np.delete(wf, obj=0, axis=0)
-Vx = waveV[:][0]
-Vz = waveV[:][2]
-
+wf2 = np.delete(wf2, obj=0, axis=0)
 plt.figure()
 plt.plot(domix, wf)
-# plt.quiver(x,z,Vx,Vz)
-plt.quiver(39.5, -10, -0.11145754, -0.0612743)
-
+plt.plot(domix, wf2)
+plt.quiver(x, z, Vx, Vz)
 plt.xlabel("X (m)")
 plt.ylabel("Z (m)")
-plt.xlim(0, 50)
+plt.xlim(0, 500)
 plt.ylim(-60, 6)
 plt.show()
