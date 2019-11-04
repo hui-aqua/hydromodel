87b0d3f0c02549d96a2a7e687fab2f21b2cd5960
hui-aqua <50576083+hui-aqua@users.noreply.github.com>
2019-11-04 08:57:23 +0100

    add a shell file for environments setting.

---
+++ etc/aliases.sh
@@ -0,0 +1,7 @@
+# Description
+#     Aliases for working with Code_Aster
+# hui.cheng@uis.no
+export APP=/home/hui/GitCode/aqua
+alias amesh='python3 $APP/apps/creatmesh.py'
+alias arun='python3 $APP/apps/allrun.py'
+alias aclean='python3 $APP/apps/allclean.py'
