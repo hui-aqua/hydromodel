c31d2b54a3aae934926db2fda04cf4785d5305c1
hui-aqua <50576083+hui-aqua@users.noreply.github.com>
2019-11-08 11:21:16 +0100

    formate the parameter,

--- apps/creatmesh.py
+++ apps/creatmesh.py
@@ -9,7 +9,7 @@ with open('cagedict', 'r') as f:
     content = f.read()
     cageinfo = eval(content)
 
-modellist = [
+modelList = [
     "fishcagewithbottom",
     "fishcagewithoutbottom",
     "squarefishcage",
@@ -20,30 +20,31 @@ modellist = [
 def printmodel():
     print("The cage type '" + cageinfo['cageType'] + "' are not included in this code.\n")
     print("Currently, the available numerical models are:")
-    for model in modellist:
-        print(str(modellist.index(model)) + " " + model)
+    for model in modelList:
+        print(str(modelList.index(model)) + " " + model)
 
-appath = input(
+
+apPath = input(
     "The default path for salome2019 is: \t/opt/salome2019/appli_V2019_univ/salome \n"
     "If your salome is located in this path, you can press '1' and enter \n\n"
     "The default path for salome2018 is: \t/opt/salome2018/appli_V2018.0.1_public/salome \n"
     "If your salome is located in this path, you can press '2' and enter \n\n"
     "Please input you path to salome in your computer \n")
-if appath == str(1):
-    appath = "/opt/salome2019/appli_V2019_univ/salome"
-if appath == str(2):
-    appath = "/opt/salome2018/appli_V2018.0.1_public/salome"
-while not os.path.isfile(appath):
-    appath = input("Sorry, I cannot find salome in your given path, please input it again \n")
-    if appath == str(1):
-        appath = "/opt/salome2019/appli_V2019_univ/salome"
-    if appath == str(2):
-        appath = "/opt/salome2018/appli_V2018.0.1_public/salome"
+if apPath == str(1):
+    apPath = "/opt/salome2019/appli_V2019_univ/salome"
+if apPath == str(2):
+    apPath = "/opt/salome2018/appli_V2018.0.1_public/salome"
+while not os.path.isfile(apPath):
+    apPath = input("Sorry, I cannot find salome in your given path, please input it again \n")
+    if apPath == str(1):
+        apPath = "/opt/salome2019/appli_V2019_univ/salome"
+    if apPath == str(2):
+        apPath = "/opt/salome2018/appli_V2018.0.1_public/salome"
 os.system("clear")
 
-modelbank = "/home/hui/GitCode/aqua/scr/Meshcreater/"
-if cageinfo['cageType'] in modellist:
-    os.system(appath + " -t " + modelbank + cageinfo['cageType'] + ".py")
+modelBank = "/home/hui/GitCode/aqua/scr/Meshcreater/"
+if cageinfo['cageType'] in modelList:
+    os.system(apPath + " -t " + modelBank + cageinfo['cageType'] + ".py")
 else:
     printmodel()
     exit()
