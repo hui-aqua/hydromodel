be017d54ad4b701b9a64396458076f5ac5a0db90
hui-aqua <50576083+hui-aqua@users.noreply.github.com>
2020-01-19 12:33:49 +0100

    move out the fis part

--- etc/aliases.sh
+++ etc/aliases.sh
@@ -6,10 +6,6 @@ export APP=/home/hui/GitCode/Code_Aster
 alias amesh='python3 $APP/apps/creatmesh.py'
 alias arun='python3 $APP/apps/allrun.py'
 alias aclean='python3 $APP/apps/allclean.py'
-alias mesh2cfd='python3 $APP/apps/mesh2CFD.py'
-alias fsirun='python3 $APP/apps/fsiRun.py'
-alias fe2cfd='python3 $APP/apps/fsi_mapCoorAndFh.py'
-alias mapVelo='python3 $APP/apps/fsi_mapvelocity.py'
 alias run='cd $HOME/aster/Run'
     
     
--- install.py
+++ install.py
@@ -46,10 +46,6 @@ export APP=''' + cwd + '''
 alias amesh='python3 $APP/apps/creatmesh.py'
 alias arun='python3 $APP/apps/allrun.py'
 alias aclean='python3 $APP/apps/allclean.py'
-alias mesh2cfd='python3 $APP/apps/mesh2CFD.py'
-alias fsirun='python3 $APP/apps/fsiRun.py'
-alias fe2cfd='python3 $APP/apps/fsi_mapCoorAndFh.py'
-alias mapVelo='python3 $APP/apps/fsi_mapvelocity.py'
 alias run='cd $HOME/aster/Run'
     
     \n''')
@@ -70,7 +66,6 @@ mesh_path = Program_path+"/scr/Meshcreater/"
 inputcreater_path = Program_path+"/scr/inputfilecreator/"
 forceModel_path = Program_path+"/scr/forcemodel/"
 seawave_path = Program_path+"/scr/enviroments/"
-FSI_path= Program_path+"/scr/FSIinterface/"
 app_path= Program_path+"/apps/"
 salome_path = "''' + salomePath + '''"
 aster_path = "''' + asterPath + '''"
@@ -80,6 +75,5 @@ output_file.close()
 os.system("ln -sf " + cwd + "/etc/workPath.py ./scr/inputfilecreator/workPath.py")
 os.system("ln -sf " + cwd + "/etc/workPath.py ./scr/Meshcreater/workPath.py")
 os.system("ln -sf " + cwd + "/etc/workPath.py ./scr/forcemodel/workPath.py")
-os.system("ln -sf " + cwd + "/etc/workPath.py ./scr/FSIinterface/workPath.py")
 os.system("ln -sf " + cwd + "/etc/workPath.py ./apps/workPath.py")
 
