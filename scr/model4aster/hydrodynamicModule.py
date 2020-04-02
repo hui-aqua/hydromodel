03ff1415f5941e8f9ae8a202f09d5650cadaa12b
hui <chenghui62000@gmail.com>
2020-04-02 10:59:55 +0200

    large updated with documentations

---
+++ scr/model4aster/hydrodynamicModule.py
@@ -0,0 +1,14 @@
+import morisonModel as morisonModel
+import screenModel as screenModel
+import wakeModel as wakeModel
+
+row = 1025  # [kg/m3]   sea water density
+kinematic_viscosity = 1.004e-6  # when the water temperature is 20 degree.
+dynamic_viscosity = 1.002e-3  # when the water temperature is 20 degree.
+
+morisonModel.row_fluid = row
+morisonModel.kinematic_viscosity = kinematic_viscosity
+morisonModel.dynamic_viscosity = dynamic_viscosity
+screenModel.row_fluid = row
+screenModel.kinematic_viscosity = kinematic_viscosity
+screenModel.dynamic_viscosity = dynamic_viscosity
