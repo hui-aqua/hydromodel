9ad24ca40b2881142dac9a2357a641cccf3da105
hui-aqua <50576083+hui-aqua@users.noreply.github.com>
2020-06-15 12:53:07 +0200

    for fishfarm Mesh creater

--- scr/Meshcreater/ME_FishFarm.py
+++ scr/Meshcreater/ME_FishFarm.py
@@ -42,18 +42,35 @@ length_of_bouncy_line = float(cageInfo['Mooring']['bouncyLine'])
 
 number_of_cage_X = int(cage_array.split("-")[0])
 number_of_cage_Y = int(cage_array.split("-")[1])
-
+mooringLines = {}
+nodesInMooring = []
 print("The number of cages in X direction are " + str(number_of_cage_X))
 print("The number of cages in X direction are " + str(number_of_cage_X))
 
 # generate the key points of frame
-point_on_fram = np.zeros((number_of_cage_X + 1, number_of_cage_Y + 1, 3))
+point_on_frame = np.zeros((number_of_cage_X + 1, number_of_cage_Y + 1, 3))
 for x in range(number_of_cage_X + 1):
     for y in range(number_of_cage_Y + 1):
-        point_on_fram[x][y] = [-length_of_frame_line / 2 * number_of_cage_X + x * length_of_frame_line,
-                               -length_of_frame_line / 2 * number_of_cage_Y + y * length_of_frame_line, -water_depth]
+        point_on_frame[x][y] = [-length_of_frame_line / 2 * number_of_cage_X + x * length_of_frame_line,
+                                -length_of_frame_line / 2 * number_of_cage_Y + y * length_of_frame_line, -water_depth]
+        nodesInMooring.append(point_on_frame[x][y])
 
 
+point_on_buoy = np.zeros((number_of_cage_X + 1, number_of_cage_Y + 1, 3))
+for x in range(number_of_cage_X + 1):
+    for y in range(number_of_cage_Y + 1):
+        point_on_buoy[x][y] = [-length_of_frame_line / 2 * number_of_cage_X + x * length_of_frame_line,
+                               -length_of_frame_line / 2 * number_of_cage_Y + y * length_of_frame_line, 0]
+
+
+
+
+
+print("the key points on frame are" + str(point_on_frame))
+
+
+
+point_on_anchor = np.zeros(((number_of_cage_X + 1+number_of_cage_Y + 1)*2, 3))
 
 
 
@@ -71,10 +88,38 @@ smesh = smeshBuilder.New(theStudy)
 Mesh_1 = smesh.Mesh()
 
 # add the pints into geometry
-for each_node in point:
+for each_node in nodesInMooring:
+    print([float(each_node[0]), float(each_node[1]), float(each_node[2])])
     nodeID = Mesh_1.AddNode(float(each_node[0]), float(each_node[1]), float(each_node[2]))
 # >>>>>>>>>>
 
+
+
+
+
+
+
+isDone = Mesh_1.Compute()
+
+# naming  the group
+# naming the node
+# GROUP_NO
+allnodes = Mesh_1.CreateEmptyGroup(SMESH.NODE, 'allnodes')
+nbAdd = allnodes.AddFrom(Mesh_1.GetMesh())
+smesh.SetName(allnodes, 'allnodes')
+
+# generate the name for each node to assign the hydrodynamic forces.
+for i in range(1, len(nodesInMooring) + 1):
+    node1 = Mesh_1.CreateEmptyGroup(SMESH.NODE, 'node%s' % i)
+    nbAdd = node1.Add([i])
+    smesh.SetName(node1, 'node%s' % i)
+
+
+
+
+
+meshname = "single_cage_" + str(sys.argv[1]).split('.')[0] + ".med"
+Mesh_1.ExportMED(cwd + "/" + meshname)
 
 meshinfo = {
     "horizontalElementLength": float(pi * cage_diameter / float(NT)),
@@ -85,7 +130,7 @@ meshinfo = {
     "netLines": con,
     "netSurfaces": sur,
     "netNodes": point,
-    "keynodes_on_frame": point_on_fram.tolist(),
+    "keynodes_on_frame": point_on_frame.tolist(),
     "NN": NN,
 }
 print("\n"
